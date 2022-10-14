197c4cf053d66555e088b16fcd55cbee7a39a734
Carlos Cerqueira <90733399+carlosvcerqueira@users.noreply.github.com>
2022-10-14 18:28:50 -0300

    updates

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
 import { Heading } from './components/Heading'
 import { Text } from './components/Text'
 import { Logo } from './Logo'
+import { TextInput } from './components/TextInput'
 import './styles/global.css'
+import { Envelope, Lock } from 'phosphor-react'
+import CheckboxStories from './components/Checkbox.stories'
+import { Checkbox } from './components/Checkbox'
+import { Button } from './components/Button'
 
  export function App() {
   return (
-    <div className='w-screen h-screen bg-gray-900 flex items-center justify-center text-gray-100'>
+    <div className='w-screen h-screen bg-gray-900 flex flex-col items-center justify-center text-gray-100'>
       <header className='flex flex-col items-center'>
         <Logo/>
 
@@ -14,8 +19,35 @@ import './styles/global.css'
         <Text size='lg' className='text-gray-400 mt-1'>Faça login e comece a usar!</Text>
       </header>
 
-      <form className='flex flex-col items-stretch'>
+      <form className='flex flex-col gap-4 items-stretch w-full max-w-sm mt-10'>
+        <label htmlFor="email" className='flex flex-col gap-3'>
+          <Text className='font-semibold'>Endereço de e-mail: </Text>
+          <TextInput.Root>
+            <TextInput.Icon>
+              <Envelope/>
+            </TextInput.Icon>
 
+            <TextInput.Input type='email' id='email' placeholder='Digite seu e-mail' />
+          </TextInput.Root>
+        </label>
+
+        <label htmlFor="password" className='flex flex-col gap-3'>
+          <Text className='font-semibold'>Senha:</Text>
+          <TextInput.Root>
+            <TextInput.Icon>
+              <Lock />
+            </TextInput.Icon>
+
+            <TextInput.Input type='password' id='password' placeholder='********'/>
+          </TextInput.Root>
+        </label>
+
+        <label htmlFor="remember" className='flex items-center gap-2'>
+          <Checkbox id='remember'/>
+          <Text size='sm' className='text-gray-200'>Lembre de mim por 30 dias</Text>
+        </label>
+
+        <Button type='submit' className='mt-4'>Entrar na plataforma</Button>
       </form>
     </div>
   )
