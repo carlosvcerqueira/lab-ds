76cf3ae925296f6e01de28d1027a288ac618e768
Carlos Cerqueira <90733399+carlosvcerqueira@users.noreply.github.com>
2022-10-14 13:36:48 -0300

    update app

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,22 @@
+import { Heading } from './components/Heading'
+import { Text } from './components/Text'
+import { Logo } from './Logo'
 import './styles/global.css'
 
  export function App() {
   return (
-    <>
-      <h1 className='font-bold text-2xl text-violet-500'>Hello World</h1>
+    <div className='w-screen h-screen bg-gray-900 flex items-center justify-center text-gray-100'>
+      <header className='flex flex-col items-center'>
+        <Logo/>
 
-      <button className='bg-cyan-300 font-medium px-4 py-2 rounde text-white hover:bg-cyan-500'>
-        Enviar
-      </button>
-    </>
-    
+        <Heading size='lg' className='mt-4'>Ignite Lab</Heading>
+
+        <Text size='lg' className='text-gray-400 mt-1'>Faça login e comece a usar!</Text>
+      </header>
+
+      <form className='flex flex-col items-stretch'>
+
+      </form>
+    </div>
   )
 }
